fe35da136d2ded81a7445eca2922e88913bc9be1
jameshly <jly32@ucmerced.edu>
2017-11-16 00:24:44 -0800

    added function outline

--- aes.cpp
+++ aes.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <vector>
+#
 using namespace std;
 
+//11/15 - started file. I started out in C++ but to be honest I might just go back to good ol' C. Exposed bits = good here. 
+//		All roundkey functions have been added with r_ prefixes.
+
+
+vector<unsigned char> roundkey(vector<unsigned char> input){
+	vector<unsigned char> output = input;
+return output;	
+}
+
+void r_rotword(){
+	
+}
+void r_subword(){
+	
+}
+void r_rcon(){
+	
+}
+void r_ek(){
+	
+}
+
+void r_k(){
+	
+}
+
+void bytesub(){
+	
+}
+
+void shiftrow(){
+	
+}
+
+void mixcol(){
+	
+}	
+
 int main(){
+	unsigned char iput = "sixteen objects";
+	vector<unsigned char> input;
+	input.push_back(iput);
 	return 0;
 }
